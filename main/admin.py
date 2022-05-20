dcae06a5dd9d28f187b750bf35da8ac8734f6047
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-05-20 13:51:05 +0300

    Add Trip model

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Trip
 
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'first_name', 'last_name', 'email', 'API_key']
 
 
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('from_city', 'to_city', 'start_date_local')
+    list_display_links = ('from_city', 'to_city',)
+
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Trip, TripAdmin)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class User(AbstractUser):
@@ -11,3 +12,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Trip(models.Model):
+    from_city = models.CharField(verbose_name='Звідки?', max_length=40)
+    to_city = models.CharField(verbose_name='Куди?', max_length=40)
+    locale = models.CharField(verbose_name='Локалізація', default='uk-UA', max_length=5)
+    currency = models.CharField(verbose_name='Валюта', default='UAH', max_length=3)
+    start_date_local = models.DateTimeField(verbose_name='Починаючи з часу', default=datetime.now)
+    end_date_local = models.DateTimeField(verbose_name='Закінчуючи часом', blank=True, null=True)
+    requested_seats = models.IntegerField(verbose_name='Кількість місць', default=1)
+    radius_in_meters = models.IntegerField(verbose_name='Радіус пошуку', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Поїздка'
+        verbose_name_plural = 'Поїздки'
+
+    def __str__(self):
+        return f'{self.from_city}-{self.to_city}: {self.start_date_local}'
