597a859478fe762889d1dd27abd135d0f98532db
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-05-28 13:29:18 +0300

    Fix task update incorrectness (after updating of the city the coordinates were not updated)

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 
     path('', views.HomePage.as_view(), name='index'),
     path('tasks/', views.TaskList.as_view(), name='task_list'),
-    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task_detail'),
+    path('task/<int:pk>/', views.TaskUpdate.as_view(), name='task_detail'),
     path('create/', views.CreateTask.as_view(), name='create_task'),
 ]
--- main/views.py
+++ main/views.py
@@ -45,15 +45,21 @@ class TaskList(LoginRequiredMixin, ListView):
         return Task.objects.filter(user=self.request.user)
 
 
-class TaskDetail(LoginRequiredMixin, UpdateView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     extra_context = {'title': 'Деталі завдання'}
     model = Task
     form_class = forms.TaskForm
     template_name = 'task_form.html'
     success_url = reverse_lazy('task_list')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.from_coordinate = form.from_city_coord
+        form.instance.to_coordinate = form.to_city_coord
+        return super(TaskUpdate, self).form_valid(form)
+
     def get_context_data(self, **kwargs):
-        context = super(TaskDetail, self).get_context_data(**kwargs)
+        context = super(TaskUpdate, self).get_context_data(**kwargs)
         task = Task.objects.get(user=self.request.user, pk=self.kwargs['pk'])
         context['url'] = Task.get_url(task)
         return context
