9c5fe2ab69e7c2212b18ec412b201fd03b099eb8
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-06-29 16:55:14 +0300

    JS active link

--- Blablacar/static/js/scripts.js
+++ Blablacar/static/js/scripts.js
@@ -1,6 +1,15 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// Add active link at navbar
+$(document).ready(function () {
+    $('.nav-item a').each(function () {
+        let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+        let link = this.href;
+        if (location === link) {
+            $(this).addClass('active');
+        }
+    })
+})
 
+// Exchange of city fields
 document.onclick = function () {
     var elem = window.event.srcElement;
 
@@ -10,4 +19,10 @@ document.onclick = function () {
         document.getElementById('to_city').value = from_city;
     }
 };
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
 
