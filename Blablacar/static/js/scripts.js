db484bdebd772fd5b0bac730138dcb0e285c90b2
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2022-12-18 11:45:13 +0200

    Added sorting and trip filter by availability of car model

--- Blablacar/static/js/scripts.js
+++ Blablacar/static/js/scripts.js
@@ -1,5 +1,21 @@
-// Add active link at navbar
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Exchange of city fields
+document.onclick = function () {
+    let elem = window.event.srcElement;
+
+    if (elem.className.includes("btn-exchange")) {
+        let from_city = document.getElementById('from_city').value;
+        document.getElementById('from_city').value = document.getElementById('to_city').value;
+        document.getElementById('to_city').value = from_city;
+    }
+};
+
 $(document).ready(function () {
+
+    // Add active link at navbar
     $('.nav-item a').each(function () {
         let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
         let link = this.href;
@@ -8,33 +24,28 @@ $(document).ready(function () {
         }
     });
 
-    // Init Isotope
-    let $grid = $('.grid').isotope({
-        itemSelector: '.trip-item',
-        layoutMode: 'fitRows'
-    });
-
-    // Store trips count
-    let iso = $grid.data('isotope');
-    let $filterCount = $('.filter-count');
-
-    function updateFilterCount() {
-        let trip_count = iso.filteredItems.length
-        $filterCount.text(trip_count);
-        if (trip_count === 0){
-            $('#empty-trip-list').removeClass('d-none');
-        }
-        else {
-            $('#empty-trip-list').addClass('d-none');
-        }
-    }
+//  ISOTOPE FILTER AND SORTING  //
 
     // City from search form
     const fromCityInput = $("input[id='from_city']")[0]['value']
     const toCityInput = $("input[id='to_city']")[0]['value']
 
+    // Store filter for each group
+    let filters = {};
+
+    // Function for update filter trip count
+    function updateFilterCount() {
+        let trip_count = iso.filteredItems.length
+        $filterCount.text(trip_count);
+        if (trip_count > 0) {
+            $('#empty-trip-list').addClass('d-none');
+        } else {
+            $('#empty-trip-list').removeClass('d-none');
+        }
+    }
+
     // Filter functions
-    let FilterFns = {
+    let filterFns = {
         from_city: function () {
             const fromCity = $(this).find('.from-city').text();
             return fromCity.match(fromCityInput);
@@ -50,42 +61,78 @@ $(document).ready(function () {
             const toCity = $(this).find('.to-city').text();
             return fromCity.match(fromCityInput) && toCity.match(toCityInput);
         },
+
+        with_vehicle: function () {
+            return $(this).find('.vehicle').text();
+        },
     };
 
+    // Init Isotope
+    let $grid = $('.grid').isotope({
+        itemSelector: '.trip-item',
+        layoutMode: 'fitRows',
+        getSortData: {
+            price: function (itemElem) {
+                var price = $(itemElem).find('.price').text();
+                return parseFloat(price.replace(/[\(\)]/g, ''));
+            }
+        },
+        filter: function () {
+
+            let isMatched = true;
+            let $this = $(this);
+
+            for (let prop in filters) {
+                let filter = filters[prop];
+                // use function if it matches
+                filter = filterFns[filter] || filter;
+                // test each filter
+                if (filter) {
+                    isMatched = isMatched && $this.is(filter);
+                }
+                // break if not matched
+                if (!isMatched) {
+                    break;
+                }
+            }
+            return isMatched;
+        }
+    });
+
+    // Store trips count
+    let iso = $grid.data('isotope');
+    let $filterCount = $('.filter-count');
+    updateFilterCount()
+
     // Bind filter button click
-    $('.filters-button-group').on('click', 'button', function () {
-        var filterValue = $(this).attr('data-filter'); // -> from_city, to_city, from_to_city, with_vehicle
-        filterValue = FilterFns[filterValue] || filterValue; // -> FUNC: from_city, to_city, from_to_city, with_vehicle
-        $grid.isotope({filter: filterValue});
+    $('.filters').on('click', 'button', function () {
+        let $this = $(this);
+        // get group key
+        let $buttonGroup = $this.parents('.button-group');
+        let filterGroup = $buttonGroup.attr('data-filter-group');
+        // set filter for group
+        filters[filterGroup] = $this.attr('data-filter');
+        // arrange, and use filter fn
+        $grid.isotope();
         updateFilterCount();
     });
 
-    updateFilterCount();
+    // Sort items on button click
+    $('.sort-by-button-group').on('click', 'button', function () {
+        let sortByValue = $(this).attr('data-sort-by');
+        $grid.isotope({sortBy: sortByValue});
+    });
 
     // Change is-checked class on buttons
     $('.button-group').each(function (i, buttonGroup) {
-        var $buttonGroup = $(buttonGroup);
+        let $buttonGroup = $(buttonGroup);
         $buttonGroup.on('click', 'button', function () {
             $buttonGroup.find('.is-checked').removeClass('is-checked');
             $(this).addClass('is-checked');
         });
     });
+    //  END ISOTOPE FILTER AND SORTING  //
 });
-
-// Exchange of city fields
-document.onclick = function () {
-    var elem = window.event.srcElement;
-
-    if (elem.className.includes("btn-exchange")) {
-        var from_city = document.getElementById('from_city').value;
-        document.getElementById('from_city').value = document.getElementById('to_city').value;
-        document.getElementById('to_city').value = from_city;
-    }
-};
-
-// Tooltip
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
 
