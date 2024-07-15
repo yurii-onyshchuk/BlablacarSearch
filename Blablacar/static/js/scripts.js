5bec795eee281d1e48fcdd0d925020482852d985
Yurii Onyshchuk <yura.onyshchuk@gmail.com>
2024-07-15 16:21:45 +0300

    Added default cities in form

--- Blablacar/static/js/scripts.js
+++ Blablacar/static/js/scripts.js
@@ -168,17 +168,60 @@ $(document).ready(function () {
     const csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
     const cache = {};
     let citySelected = false;
+    const primaryCities = [
+        {
+            Description: 'Київ', Latitude: '50.450418000000000', Longitude: '30.523541000000000',
+            SettlementTypeDescription: 'м.', AreaDescription: 'Київська'
+        },
+        {
+            Description: 'Львів', Latitude: '49.839678000000000', Longitude: '24.029709000000000',
+            SettlementTypeDescription: 'м.', AreaDescription: 'Львівська'
+        },
+        {
+            Description: 'Одеса', Latitude: '46.484579000000000', Longitude: '30.732597000000000',
+            SettlementTypeDescription: 'м.', AreaDescription: 'Одеська'
+        },
+        {
+            Description: 'Харків', Latitude: '49.992167000000000', Longitude: '36.231202000000000',
+            SettlementTypeDescription: 'м.', AreaDescription: 'Харківська'
+        },
+        {
+            Description: 'Рівне', Latitude: '50.619898000000000', Longitude: '26.251611000000000',
+            SettlementTypeDescription: 'м.', AreaDescription: 'Рівненська'
+        },
+        {
+            Description: 'Вінниця', Latitude: '49.233118000000000', Longitude: '28.468231000000000',
+            SettlementTypeDescription: 'м.', AreaDescription: 'Вінницька'
+        },
+        {
+            Description: 'Житомир', Latitude: '50.254623000000000', Longitude: '28.658692000000000',
+            SettlementTypeDescription: 'м.', AreaDescription: 'Житомирська'
+        },
+        {
+            Description: 'Ізяслав', Latitude: '50.119112400000000', Longitude: '26.828114100000000',
+            SettlementTypeDescription: 'м.', AreaDescription: 'Хмельницька'
+        }
+    ];
+
 
     // Autocomplete function
     function setupAutocomplete(inputSelector, resultsSelector, coordinateField) {
         $(inputSelector).on('click', function () {
             citySelected = false;
             $(coordinateField).attr('value', '');
-            debounceAutocomplete($(this).val(), inputSelector, resultsSelector);
+            if (!$(this).val()) {
+                displayPrimaryCities(resultsSelector);
+            } else {
+                debounceAutocomplete($(this).val(), inputSelector, resultsSelector);
+            }
         }).on('input', debounce(function () {
             if (!citySelected) {
                 $(coordinateField).attr('value', '');
-                debounceAutocomplete($(this).val(), inputSelector, resultsSelector);
+                if (!$(this).val()) {
+                    displayPrimaryCities(resultsSelector);
+                } else {
+                    debounceAutocomplete($(this).val(), inputSelector, resultsSelector);
+                }
             }
         }, 300));
 
@@ -231,7 +274,7 @@ $(document).ready(function () {
                 });
             }
         } else {
-            $(resultsSelector).hide();
+            displayPrimaryCities(resultsSelector);
         }
     }
 
@@ -239,6 +282,14 @@ $(document).ready(function () {
         $(resultsSelector).empty();
         $(resultsSelector).show();
         $.each(data, function (index, city) {
+            $(resultsSelector).append(`<li class="dropdown-item" data-city="${city.Description}" data-latitude="${city.Latitude}" data-longitude="${city.Longitude}">${city.SettlementTypeDescription.substring(0, 1)}. ${city.Description}, ${city.AreaDescription} обл.</li>`);
+        });
+    }
+
+    function displayPrimaryCities(resultsSelector) {
+        $(resultsSelector).empty();
+        $(resultsSelector).show();
+        $.each(primaryCities, function (index, city) {
             $(resultsSelector).append(`<li class="dropdown-item" data-city="${city.Description}" data-latitude="${city.Latitude}" data-longitude="${city.Longitude}">${city.SettlementTypeDescription.substring(0, 1)}. ${city.Description}, ${city.AreaDescription} обл.</li>`);
         });
     }
